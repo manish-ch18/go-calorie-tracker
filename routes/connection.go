d430a585a5a75b5e162c2a99d49cab47b1e91177
manish-ch18 <manish.ch1812@gmail.com>
2024-06-02 09:43:01 +0530

    update project

---
+++ routes/connection.go
@@ -0,0 +1,40 @@
+package routes
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBInstance() *mongo.Client {
+	MongoDB := "mongodb://localhost:27017/caloriesdb"
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(MongoDB))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connectted to database")
+
+	return client
+}
+
+var Client *mongo.Client = DBInstance()
+
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("caloriesdb").Collection(collectionName)
+
+	return collection
+}
